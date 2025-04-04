c59db30a41777fd0321b27ec3ad52e498721de7f
Andres <andressanchez1816@gmail.com>
2025-04-03 21:20:20 -0300

    second commit

--- pruebas.js
+++ pruebas.js
@@ -1,8 +1,42 @@
 // const _ = require('lodash')
-const utils = require("./utils.js")
+// const utils = require("./utils.js")
 
-let lista = ([[1,2],3,[4,5,6],7,[8,9]])
+// let lista = ([[1,2],3,[4,5,6],7,[8,9]])
 
-const fletten_array = utils.Fn(lista)
+// const fletten_array = utils.Fn(lista)
 
-console.log(utils.ultimo(fletten_array))
+// console.log(utils.ultimo(fletten_array))
+
+const express = require("express")
+const { json } = require("express/lib/response")
+
+let cursos = [
+    {
+        id:1,
+        nombre:"POO",
+        cantidad_alumnos:54
+    },
+
+    {
+        id:2,
+        nombre:"Optativa",
+        cantidad_alumnos:25
+    }
+]
+
+const app = express()
+
+app.get("/cursos", (req, res) =>{
+    res.status(200).json({
+        cursos
+    })
+})
+
+app.post("/cursos", (req, res) => {
+    cursos.push(req)
+    res.status(201)
+})
+
+app.listen(4001, () => {
+    console.log("La aplicacion arranco en el puerto 4001")
+})
--- utils.js
+++ utils.js
@@ -1,4 +1,5 @@
 const _ = require('lodash')  // "_" podria llamarse como quieras.
+                            //Exportas todo el contenido de la libreria lodash dentro de node-modules
 
 const Fn = (arr) => {
     return _.flatten(arr)
@@ -8,4 +9,4 @@ const last = (arr) => {
     return _.last(arr)
 }
 
-module.exports = {Fn, ultimo:last}
+module.exports = {Fn, ultimo:last}  //Que funciones se van a exportar, las que no esten no se exportan
